b10788146777c3d66dc262d2cdecdab8f10bb4cd
hifichevymane <blekbjek@gmail.com>
2024-12-22 14:23:13 +0200

    init project + navbar comp

---
+++ src/components/BrowseDropdown.vue
@@ -0,0 +1,60 @@
+<script setup></script>
+
+<template>
+  <div class="wrapper">
+    <ul class="category-list">
+      <li class="category-list-item">Popular</li>
+      <li class="category-list-item">New</li>
+      <li class="category-list-item">Alphabetical</li>
+      <li class="category-list-item">Simulcast Season</li>
+      <li class="category-list-item">Release Calendar</li>
+      <li class="category-list-item">Music Videos & Concerts</li>
+    </ul>
+    <div class="genres">
+      <span class="genres-title">GENRES</span>
+      <ul class="genre-list">
+        <li class="genre-list-item">Action</li>
+        <li class="genre-list-item">Adventure</li>
+        <li class="genre-list-item">Comedy</li>
+        <li class="genre-list-item">Drama</li>
+        <li class="genre-list-item">Fantasy</li>
+        <li class="genre-list-item">Music</li>
+        <li class="genre-list-item">Romance</li>
+        <li class="genre-list-item">Sci-Fi</li>
+        <li class="genre-list-item">Seinen</li>
+        <li class="genre-list-item">Shojo</li>
+        <li class="genre-list-item">Shonen</li>
+        <li class="genre-list-item">Slice of life</li>
+        <li class="genre-list-item">Sports</li>
+        <li class="genre-list-item">Supernatural</li>
+        <li class="genre-list-item">Thriller</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  @apply absolute top-14 flex bg-gray-900 text-gray-200 py-2 z-10;
+}
+
+.category-list {
+  @apply w-56 flex flex-col bg-gray-900 text-gray-200 border-r-2 border-gray-600;
+}
+
+.category-list-item {
+  @apply cursor-pointer py-2 px-4 h-10 hover:bg-gray-800;
+}
+
+.genre-list {
+  @apply grid grid-cols-3;
+}
+
+.genre-list-item {
+  @apply w-52 h-10 leading-10 cursor-pointer px-4 hover:bg-gray-800;
+}
+
+.genres-title {
+  @apply text-gray-400 block py-2 px-4;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<script setup>
+import BrowseDropdown from './BrowseDropdown.vue'
+import { ref } from 'vue'
+
+const browseDropdownOpened = ref(false)
+
+const onClickHandler = () => {
+  browseDropdownOpened.value = !browseDropdownOpened.value
+}
+</script>
+
+<template>
+  <ul class="navbar">
+    <li class="nav-item" :class="[browseDropdownOpened && 'bg-gray-900']" @click="onClickHandler">
+      <span>Browse</span>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <path fill="currentColor" d="m12 15l5-5H7z" />
+      </svg>
+    </li>
+    <BrowseDropdown v-show="browseDropdownOpened" />
+    <li class="nav-item">Games</li>
+    <li class="nav-item">
+      <span>News</span>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <path fill="currentColor" d="m12 15l5-5H7z" />
+      </svg>
+    </li>
+  </ul>
+  <div class="overlay" v-show="browseDropdownOpened" @click="onClickHandler"></div>
+</template>
+
+<style scoped>
+.overlay {
+  @apply fixed top-0 right-0 left-0 bottom-0 z-0 w-full h-full bg-gray-950/80;
+}
+
+.navbar {
+  @apply bg-gray-800 flex relative z-30;
+}
+
+.nav-item {
+  @apply px-5 py-4 font-main cursor-pointer text-gray-200
+  flex items-center gap-1 select-none transition-colors hover:bg-gray-900;
+}
+</style>
