7779ece3cbceb90d67acbcb19585ffab69e5f22c
hifichevymane <blekbjek@gmail.com>
2024-12-28 22:52:54 +0200

    different description and logo for slides

--- src/components/ImageCarousel.vue
+++ src/components/ImageCarousel.vue
@@ -4,19 +4,36 @@ import { ref } from 'vue'
 const activeSlideIdx = ref(0)
 const slides = ref([
   {
-    src: '/src/assets/img/demon_slayer_season_3_release_slide.webp',
+    src: 'src/assets/img/demon_slayer_season_3_release_slide.webp',
     alt: 'Demon Slayer Anime',
-    logo: '/src/assets/img/Demon-Slayer-Logo.png',
+    logo: 'src/assets/img/Demon-Slayer-Logo.png',
+    description: `
+      After his sister is turned into a demon,
+      Tanjiro Kamado joins the Demon Slayer Corps to save her and fight deadly demons.
+      With stunning animation and an emotional story,
+      Demon Slayer is a powerful tale of courage and family.
+    `,
   },
   {
-    src: '/src/assets/img/Jujutsu_Kaisen.webp',
+    src: 'src/assets/img/Jujutsu_Kaisen.webp',
     alt: 'Jujutsu Kaisen Anime',
-    logo: '/src/assets/img/Demon-Slayer-Logo.png',
+    logo: 'src/assets/img/Jujutsu_Kaisen_logo.png',
+    description: `
+      When high schooler Yuji Itadori swallows a cursed object,
+      he gains extraordinary powers and joins Jujutsu High to battle deadly cursed spirits.
+      With unforgettable characters and intense action,
+      Jujutsu Kaisen delivers a supernatural adventure you won’t want to miss!
+    `,
   },
   {
-    src: '/src/assets/img/Anime_BlueLock_Season2KeyArt.jpg',
+    src: 'src/assets/img/Anime_BlueLock_Season2KeyArt.jpg',
     alt: 'Blue Lock Anime',
-    logo: '/src/assets/img/Demon-Slayer-Logo.png',
+    logo: 'src/assets/img/Blue_Lock_Logo.png',
+    description: `
+      Japan’s desire for World Cup glory leads the Japanese Football Association to launch a
+      new rigorous training program to find the national team’s next striker. Three hundred
+      high school players are pitted against each other
+    `,
   },
 ])
 
@@ -62,11 +79,7 @@ const onNextBtnClick = () => {
         <img class="image-background" :src="slide.src" :alt="slide.alt" />
         <div class="slide-content">
           <img class="anime-logo" :src="slide.logo" alt="Anime Logo" />
-          <p class="slide-description">
-            Japan’s desire for World Cup glory leads the Japanese Football Association to launch a
-            new rigorous training program to find the national team’s next striker. Three hundred
-            high school players are pitted against each other
-          </p>
+          <p class="slide-description">{{ slide.description }}</p>
           <div class="slide-buttons">
             <button class="start-watching-btn">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
